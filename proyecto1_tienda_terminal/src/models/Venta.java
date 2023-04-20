7124b0b4ede4b6734846fe9c37751e30ac41473d
Alan Badillo Salas <dragonnomada123@gmail.com>
2023-04-20 17:00:04 -0600

    Actualizando proyecto 1

--- proyecto1_tienda_terminal/src/models/Venta.java
+++ proyecto1_tienda_terminal/src/models/Venta.java
@@ -1,7 +1,6 @@
 package models;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Venta {
@@ -9,10 +8,55 @@ public class Venta {
     long id;
     Tienda tienda;
     ArrayList<Producto> productos;
-    LocalDate fechaAbierto;
-    LocalTime fechaCerrado;
+    LocalDateTime fechaAbierto;
+    LocalDateTime fechaCerrado;
     double total;
     boolean estaPagado;
     boolean estaCompletado;
+    boolean estaCancelado;
+
+    public Venta(long id, Tienda tienda) {
+        this.id = id;
+        this.tienda = tienda;
+        this.productos = new ArrayList<>();
+        this.fechaAbierto = LocalDateTime.now();
+        this.fechaCerrado = null;
+        this.total = 0.0;
+        this.estaPagado = false;
+        this.estaCompletado = false;
+        this.estaCancelado = false;
+    }
+
+    public void registrarProducto(long productoId) {
+        Producto producto = this.tienda.buscarProducto(productoId);
+        if (producto != null) {
+            this.productos.add(producto);
+            this.calcularTotal();
+        }
+    }
+
+    public void calcularTotal() {
+        this.total = 0.0;
+        for (Producto producto : this.productos) {
+            this.total = this.total + producto.getPrecio();
+            // total += producto.getPrecio();
+        }
+    }
+
+    public double pagar(double monto) {
+        if (monto >= this.total) {
+            double cambio = monto - this.total;
+            this.estaPagado = true;
+            this.estaCompletado = true;
+            return cambio;
+        } else {
+            return monto;
+        }
+    }
+
+    public void cancelar() {
+        this.estaCancelado = true;
+        this.estaCompletado = true;
+    }
 
 }
